44c444410396c82bbbe1bd45b3fa523a5ee4f1d1
DomovoiSasha <domovoi_sasha@mail.ru>
2018-06-05 16:37:21 +0500

    html and styles Price-list, little content slider on mobile

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -3,6 +3,11 @@
 	
 	const menuBtn = $('.main-nav__btn');
 	const menu = $('.main-nav__menu');
+	const prevPlan = $('.price__btn-prev');
+	const nextPlan = $('.price__btn-next');
+	const slideshowLength = $('.price-plan').length;
+	const firstSlide = $('.price-plan').eq(0);
+	const lastSlide = $('.price-plan').eq(slideshowLength - 1);
 	
 	$(menuBtn).click(function(e){
 		$(menu).stop().slideToggle(400);
@@ -20,7 +25,7 @@
 		gallery: {
 			enabled: true,
 			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+			preload: [0,1] 
 		},
 		image: {
 			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
@@ -29,5 +34,29 @@
 			}
 		}
 	});
+	
+	
+	$(nextPlan).bind("click", function(e){
+		let currentSlide = $('.price-plan:visible');
+		let nextSlide = $($(currentSlide).next());
+		$(currentSlide).hide();
+		if($(currentSlide).index() == slideshowLength) {
+			$(firstSlide).show();
+		} else {
+			$(nextSlide).show();
+		}
+	});
+	
+	$(prevPlan).bind("click", function(e){
+		let currentSlide = $('.price-plan:visible');
+		let prevSlide = $($(currentSlide).prev());
+		$(currentSlide).hide();
+		if($(currentSlide).index() == 1) {
+			$(lastSlide).show();
+		} else {
+			$(prevSlide).show();
+		}
+	});
+	
 	
 })()
